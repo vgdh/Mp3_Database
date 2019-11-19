f20757d0d8f02f11f35a47b3f582742fdef7dd9b
IR <IR@IR>
2019-11-20 00:20:51 +0500

    add sqlite entity model

---
+++ Mp3_Database/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Mp3_Database.Model;
+
+namespace Mp3_Database
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApplicationContext db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new ApplicationContext();
+            db.songs.Load();
+            this.DataContext = db.songs.Local.ToBindingList();
+
+        }
+
+    }
+}
---
+++ Mp3_Database/Model/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mp3_Database.Model
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("DefaultConnection")
+        {
+            using (mainEntities db = new mainEntities())
+            {
+                song s1 = new song()
+                {
+                    artist = "artist 1",
+                    title = "title 1"
+                };
+                db.song.Add(s1);
+                db.SaveChanges();
+            }
+        }
+
+    }
+}

e6f0266e2dd748727222f43cc529c026712b185c
IR <IR@IR>
2019-11-28 15:46:55 +0500

    Элементы управления базой и копированием файлов

--- Mp3_Database/MainWindow.xaml.cs
+++ Mp3_Database/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -24,29 +26,23 @@ namespace Mp3_Database
     /// </summary>
     public partial class MainWindow : Window
     {
+        private mainEntities1 db = new mainEntities1();
+
         public MainWindow()
         {
             InitializeComponent();
-
-
-            using (var db = new mainEntities1())
-            {
-                //db.Songs.Add(new Song("1", "2", 123));
-                //db.SaveChanges();
-                db.Songs.Load();
-                this.ListViewExistingSongs.ItemsSource = db.Songs.Local;
-            }
+            db.Songs.Load();
+            this.ListViewExistingSongs.ItemsSource = db.Songs.Local;
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(ListViewExistingSongs.ItemsSource);
+            view.SortDescriptions.Add(new SortDescription("Artist", ListSortDirection.Ascending));
+            view.SortDescriptions.Add(new SortDescription("Title", ListSortDirection.Ascending));
         }
 
         private void ListViewNewSongs_OnDrop(object sender, DragEventArgs e)
         {
             string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
             List<string> filePathsList = GetFilePathsRecursive(files);
-        
             ListViewNewSongs.ItemsSource = CreateListOfSongs(filePathsList);
-
-
-
         }
 
         private List<string> GetFilePathsRecursive(string[] files)
@@ -57,12 +53,12 @@ namespace Mp3_Database
             {
                 if (Directory.Exists(file))
                 {
-                    string[] filePathsInDir = Directory.GetFiles(file);
+                    string[] filePathsInDir = Directory.GetFiles(file, "*", SearchOption.AllDirectories);
                     filePathsListForReturn.AddRange(GetFilePathsRecursive(filePathsInDir));
                 }
                 else
                 {
-                    if (file.EndsWith(".mP3",StringComparison.CurrentCultureIgnoreCase))
+                    if (file.EndsWith(".mP3", StringComparison.CurrentCultureIgnoreCase))
                     {
                         filePathsListForReturn.Add(file);
                     }
@@ -80,19 +76,77 @@ namespace Mp3_Database
                 using (var mp3 = new Mp3(filePath))
                 {
                     Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X);
-                   
-                    if (string.IsNullOrEmpty(tag.Band) || string.IsNullOrEmpty(tag.Title))
+                    Id3Tag tag1 = mp3.GetTag(Id3TagFamily.Version1X);
+
+                    if (string.IsNullOrEmpty(tag.Title) || string.IsNullOrEmpty(tag.Artists.Value[0]))
                     {
-                        MessageBox.Show($"Трек {filePath} не содержит в себе MP3 tag исполнителя или название трека");
+                        MessageBox.Show($"Трек {filePath} MP3 tag не содержит в себе название трека");
                     }
                     else
                     {
-                        Song newSong = new Song(tag.Band, tag.Title, (int)DateTime.Now.Ticks);
+                        var unixTime = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+
+                        Song newSong = new Song(tag.Artists.Value[0], tag.Title, unixTime, filePath);
                         songs.Add(newSong);
                     }
                 }
             }
             return songs;
+        }
+
+        private void Button_Click_CopySongs(object sender, RoutedEventArgs e)
+        {
+            if (ListViewNewSongs.ItemsSource != null)
+            {
+                foreach (Song song in ListViewNewSongs.ItemsSource)
+                {
+                    if (db.Songs.Count(x => x.Title == song.Title & x.Artist == song.Artist) == 0)
+                    {
+                        CopyNewSongsToOutputFolder(song);
+                        db.Songs.Add(song);
+                    }
+                }
+                db.SaveChanges();
+            }
+            Process.Start(@".\\MP3_Output\\");
+        }
+
+        private void CopyNewSongsToOutputFolder(Song song)
+        {
+            if (!Directory.Exists(".\\MP3_Output"))
+            {
+                Directory.CreateDirectory(".\\MP3_Output");
+            }
+
+            File.Copy(song.FilePath, $".\\MP3_Output\\{song.Artist} - {song.Title}.mp3", overwrite: true);
+        }
+
+        private void Button_Click_RemoveSongFromDatabase(object sender, RoutedEventArgs e)
+        {
+            var songs = ListViewExistingSongs.SelectedItems.Cast<Song>().ToList();
+            if (songs.Count > 0)
+            {
+                db.Songs.RemoveRange(songs);
+                db.SaveChanges();
+            }
+
+        }
+
+        private void Button_Click_AddSongsToDatabase(object sender, RoutedEventArgs e)
+        {
+            if (ListViewNewSongs.ItemsSource != null)
+            {
+                foreach (Song song in ListViewNewSongs.ItemsSource)
+                {
+                    if (db.Songs.Count(x => x.Title == song.Title & x.Artist == song.Artist) == 0)
+                    {
+                        db.Songs.Add(song);
+                    }
+                }
+                db.SaveChanges();
+            }
+
+            MessageBox.Show("Дреки добалены в базу данных");
         }
     }
 
@@ -115,5 +169,6 @@ namespace Mp3_Database
 
         public string FilePath { get; set; }
 
+        public DateTime AddTimeDateTime => (new DateTime(1970, 1, 1, 0, 0, 0, 0)).AddSeconds(this.Add_time);
     }
 }

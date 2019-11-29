7024c1d28e4b0f3fe48e57c2b66bd86102c2cbbd
IR <IR@IR>
2019-11-29 22:10:44 +0500

    EF SQLite запустился с отдельного солюшена

--- Mp3_Database/View/MainWindow.xaml.cs
+++ Mp3_Database/View/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Data.Entity;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
